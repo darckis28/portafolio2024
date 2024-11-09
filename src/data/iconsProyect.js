a3182012d5d6c828f88f19ad8be298005b206c47
andi aguirre <darckisgamer@gmail.com>
2024-11-09 14:50:30 -0500

    add link and proyect moviedev

--- src/data/iconsProyect.js
+++ src/data/iconsProyect.js
@@ -3,12 +3,15 @@ import astro from '../assets/images/astro.svg'
 import html from "../assets/images/html.svg"
 import css from '../assets/images/css.svg'
 import tailwind from '../assets/images/tailwind.svg'
+import github from "../assets/images/github.svg"
 const iconsProyects = {
     react,
     astro,
     tailwind,
     css,
     html,
+    github
+
 
 }
 export default iconsProyects
--- src/data/proyects.js
+++ src/data/proyects.js
@@ -1,12 +1,13 @@
 
 import proyect1 from "../assets/images/avatar.webp";
+import movieDev from "../assets/images/movieDev.webp";
 const proyects = [
     {
-        title: "Proyecto 1",
-        description: "loremdsadmsakdmsad",
-        link: "#",
-        image: proyect1,
-        technologies: ['react', 'astro']
+        title: "MovieDev",
+        description: "Movie Dev es una página de búsqueda de películas, con integraciones de API para mostrar detalles de cada título, y un diseño responsive que se adapta a todos los dispositivos.",
+        link: "https://movie-dev-one.netlify.app",
+        image: movieDev,
+        technologies: ['react', 'tailwind', 'css', 'html']
     },
     {
         title: "Proyecto 1",
