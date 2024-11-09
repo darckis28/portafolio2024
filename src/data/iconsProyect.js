2287fde1d6ebacc9ce4722dd992ac8da94d031ac
andi aguirre <darckisgamer@gmail.com>
2024-11-08 22:51:48 -0500

    add progresing porfolio

---
+++ src/data/iconsProyect.js
@@ -0,0 +1,14 @@
+import react from '../assets/images/react.svg'
+import astro from '../assets/images/astro.svg'
+import html from "../assets/images/html.svg"
+import css from '../assets/images/css.svg'
+import tailwind from '../assets/images/tailwind.svg'
+const iconsProyects = {
+    react,
+    astro,
+    tailwind,
+    css,
+    html,
+
+}
+export default iconsProyects
---
+++ src/data/proyects.js
@@ -0,0 +1,26 @@
+
+import proyect1 from "../assets/images/avatar.webp";
+const proyects = [
+    {
+        title: "Proyecto 1",
+        description: "loremdsadmsakdmsad",
+        link: "#",
+        image: proyect1,
+        technologies: ['react', 'astro']
+    },
+    {
+        title: "Proyecto 1",
+        description: "loremdsadmsakdmsad",
+        link: "#",
+        image: proyect1,
+        technologies: ['react', 'tailwind']
+    },
+    {
+        title: "Proyecto 1",
+        description: "loremdsadmsakdmsad",
+        link: "#",
+        image: proyect1,
+        technologies: ['react', 'html', 'css']
+    },
+];
+export default proyects
